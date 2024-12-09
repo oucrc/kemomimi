cf8ac4254dd0752d4339ae7865d07014ae7750b1
hinshiba <105423175+hinshiba@users.noreply.github.com>
2024-12-09 22:40:19 +0900

    add: main.rs

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    extract::{Path, Query, State},
+    http::StatusCode,
+    routing::{delete, get, patch, post},
+    Json, Router,
+};
+use serde::{Deserialize, Serialize};
+use sqlx::SqlitePool;
+use std::sync::Arc;
+use std::{collections::HashMap, env};
+
+#[tokio::main]
+async fn main() {
+    dotenvy::dotenv().expect("Failed to read .env file");
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    // initialize tracing
+    tracing_subscriber::fmt::init();
+
+    // TODO postgresへの移行
+    let pool = Arc::new(SqlitePool::connect(&database_url).await.unwrap());
+    // build our application with a route
+    let app = Router::new().with_state(pool.clone());
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
