42ae3302013d1c543af673ff1ae9532e065b60ff
gomazarashi <Seto42357@gmail.com>
2024-12-16 16:55:49 +0900

    add: 開発ロードマップの第1段階に対応するmigrationを作成

--- backend/migrations/20241216163427_first-kemomimi.down.sql
+++ backend/migrations/20241216163427_first-kemomimi.down.sql
@@ -1,1 +1,11 @@
--- Add down migration script here
+-- 製品-カテゴリ関係テーブル（ProductCategory）削除
+DROP TABLE ProductCategory;
+
+-- 分類テーブル（Category）削除
+DROP TABLE Category;
+
+-- 備品テーブル（PublicItem）削除
+DROP TABLE PublicItem;
+
+-- 製品テーブル（Product）削除
+DROP TABLE Product;　
--- backend/migrations/20241216163427_first-kemomimi.up.sql
+++ backend/migrations/20241216163427_first-kemomimi.up.sql
@@ -1,1 +1,42 @@
--- Add up migration script here
+-- 製品テーブル（Product）作成
+CREATE TABLE
+    Product (
+        product_id TEXT PRIMARY KEY, -- 製品のユニークID
+        name TEXT NOT NULL, -- 製品名
+        model_number TEXT, -- 型番
+        product_url TEXT, -- 商品URL
+        remarks TEXT -- 備考
+    );
+
+-- 備品テーブル（PublicItem）作成
+CREATE TABLE
+    PublicItem (
+        public_item_id TEXT PRIMARY KEY, -- 備品のユニークID
+        name TEXT NOT NULL, -- 備品名
+        product_id TEXT, -- 製品のユニークID（Foreign Key）
+        cost INT CHECK (cost >= 0), -- 購入コスト
+        purchase_date DATE NOT NULL, -- 導入日
+        expiration_date DATE CHECK (expiration_date > purchase_date), -- 耐用期限
+        is_remaining BOOLEAN DEFAULT TRUE NOT NULL, -- 現存状態（廃棄済みなど）
+        purchase_request_id TEXT, -- 購入申請ID（Foreign Key）
+        remarks TEXT, -- 備考
+        FOREIGN KEY (product_id) REFERENCES Product (product_id) -- 製品IDへの外部キー
+    );
+
+-- 分類テーブル（Category）作成
+CREATE TABLE
+    Category (
+        category_id TEXT PRIMARY KEY, -- カテゴリのユニークID
+        name TEXT UNIQUE NOT NULL, -- カテゴリ名
+        remarks TEXT -- 備考
+    );
+
+-- 製品-カテゴリ関係テーブル（ProductCategory）作成
+CREATE TABLE
+    ProductCategory (
+        product_id TEXT, -- 製品のID（ProductへのForeign Key）
+        category_id TEXT, -- カテゴリのID（CategoryへのForeign Key）
+        PRIMARY KEY (product_id, category_id), -- 複合主キー
+        FOREIGN KEY (product_id) REFERENCES Product (product_id), -- 製品IDへの外部キー
+        FOREIGN KEY (category_id) REFERENCES Category (category_id) -- カテゴリIDへの外部キー
+    );
